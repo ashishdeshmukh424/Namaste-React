846337df3230ea1b6997b0225a4fc0665f7c62c9
Ashish Deshmukh <deshmukh.ashish424@gmail.com>
2024-01-12 23:32:58 +0530

    First commit after ep01

---
+++ App.js
@@ -0,0 +1,29 @@
+// const heading = React.createElement("h1", {}, "Helloo World From React!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+
+// ================================================
+// // write below code using react
+
+// // <div id="parent">
+// //     <div id="child">
+// //         <h1> Hi from child 1</h1>
+// //         <h1> Hi from child 2</h1>
+// //     </div>
+
+// // </div>
+
+// const parent = React.createElement("div", {id:"parent"}, 
+// React.createElement("div", {id:"child"}, [
+//     React.createElement("h1", {id:"heading1"}, "Hi from child 1"),
+//     React.createElement("h2", {id:"heading2"}, "Hi from child 2")
+// ])
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(parent);
+
+// ================================================================
+
